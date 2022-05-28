58a5d220a9b0aac4322554baa400fd88e5bbe9da
gio acevedo <gio.acevedoa@gmail.com>
2022-05-27 22:48:18 -0400

    cambio en historial

--- php/miregistro.php
+++ php/miregistro.php
@@ -278,17 +278,17 @@
                     <div class="col-sm-12 col-md-6 col-xl-4">
                         <div class="h-100 bg-light rounded p-4">
                             <div class="d-flex align-items-center justify-content-between mb-2">
-                                <h6 class="mb-0">Messages</h6>
-                                <a href="">Show All</a>
+                                <h6 class="mb-0">Historia de consulta</h6>
+                                <a href="">Todo</a>
                             </div>
                             <div class="d-flex align-items-center border-bottom py-3">
-                                <img class="rounded-circle flex-shrink-0" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                                <img class="rounded-circle flex-shrink-0" src="miregistro/img/doc1.jpeg" alt="" style="width: 40px; height: 40px;">
                                 <div class="w-100 ms-3">
                                     <div class="d-flex w-100 justify-content-between">
-                                        <h6 class="mb-0">Jhon Doe</h6>
-                                        <small>15 minutes ago</small>
+                                        <h6 class="mb-0">Dra. Constanza</h6>
+                                        <small>15 Agosto  2022</small>
                                     </div>
-                                    <span>Short message goes here...</span>
+                                    <span>Kiara presenta dolor abdominal...</span>
                                 </div>
                             </div>
                             <div class="d-flex align-items-center border-bottom py-3">
